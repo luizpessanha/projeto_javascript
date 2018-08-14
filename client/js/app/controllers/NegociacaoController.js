833772cd7b19ad80341c64c01098418f7c3a2328
Luiz Pessanha <luizcarp@gmail.com>
2018-08-14 06:42:51 -0300

    Atualizaçao de form

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -5,27 +5,36 @@ class NegociacaoController {
         this._inputData = $("#data");
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
+        this._listaNegociacoes = new ListaNegociacoes();
+        this._negociacoesView = new NegociacoesView();
 
     }
 
     adiciona(event){
         event.preventDefault();
-        let helper = new DateHelpers();
         
-
-        let negociacao = new Negociacao(
-            helper.textoParaData(this._inputData.value),
-            this._inputQuantidade.value,
-            this._inputValor.value
-        )
-
-
-        console.log(helper.dataParaTexto(negociacao.data));
+        this._listaNegociacoes.adiciona(this._criarNegociacao());
+        this._limpaFormulario;
+        console.log(DateHelpers.dataParaTexto(negociacao.data));
 
         console.log(negociacao);
-    
-
         
+    }
+
+    _limpaFormulario(){
+        form = document.querySelector(".form");
+        form.reset();
+        this._inputData.focus;
+
+    }
+    _criarNegociacao(){
+        return new Negociacao(
+                DateHelpers.textoParaData(this._inputData.value),
+                this._inputQuantidade.value,
+                this._inputValor.value
+            );
+        )
+
     }
 
 }
